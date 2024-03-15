8a145d2117b415e5ee4edea6ef894a9d585f7a67
Imad Eddine CHACHOUA <70714787+nof1eld@users.noreply.github.com>
2024-03-15 01:41:41 +0100

    Update p.c

--- p.c
+++ p.c
@@ -2,6 +2,18 @@
 #include<stdlib.h>
 
 int main(){
-    
+    int X;
+    printf("Enter X: ");
+    scanf("%d", &X);
+    int *pX = &X;
+    int Y = *pX;
+    //the value of X:
+    printf("X: %d\n", X);
+    //the address of X (pX holds the address while *pX holds the value):
+    printf("X address(pX): %p\n", pX);
+    //the value of Y(= the value of X):
+    printf("Y: %d\n", Y);
     return 0;
 }
+//References: https://youtu.be/2ybLD6_2gKM?si=HGBQJKp8W8M4CagR, 
+//MS Copilot
